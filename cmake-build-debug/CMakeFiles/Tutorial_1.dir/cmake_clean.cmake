6a85036613500feff97138c02779a07d24c80fa7
Ayusch Jain <ayusch@Ayuschs-MacBook-Pro.local>
2021-01-24 14:08:58 +0530

    First commit

---
+++ cmake-build-debug/CMakeFiles/Tutorial_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial_1.dir/main.cpp.o"
+  "Tutorial_1"
+  "Tutorial_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int choice = 0;
+    cout <<"Enter your choice: \n1. Happy birthday \n 2. Happy Anniversary"<<endl;
+    cin >>choice;
+    switch (choice) {
+        case 1:
+            cout << "Happy birthday, many wishes of the day."<<endl;
+            break;
+        case 2:
+            cout << "Happy Anniversary, many wishes."<<endl;
+            break;
+        default:
+            cout << "Enter the correct choice!";
+    }
+    return 0;
+}
